57e5b321d8d8e7acf5ca63c2b87ee6653ee2c492
duchaochen <520duchaochen>
2018-09-23 10:50:04 +0800

    Initial commit

---
+++ src/main/java/com/adu/springboot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.adu.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String helloWorld() {
+        return "helloWorld...";
+    }
+}
